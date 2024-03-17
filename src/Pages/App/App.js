e1d538663d6e36df2c2d66c72eda449b200a7acf
Cesar Contreras <contreras.cesar53@gmail.com>
2024-03-16 23:32:36 -0500

    se maqueta primera version de la gestion de usuarios

--- src/Pages/App/App.js
+++ src/Pages/App/App.js
@@ -14,6 +14,7 @@ import { Success } from '../Success/Success';
 import './App.css';
 import { NavBar } from '../../Components/NavBar/NavBar';
 import { SignIn } from '../SignIn/SignIn';
+import { Usuarios } from '../usuarios/Usuarios';
 
 const AppRoutes = () => {
   let routes = useRoutes([
@@ -27,6 +28,7 @@ const AppRoutes = () => {
     { path: '/shop', element: <Shop /> },
     { path: '/shopping-car', element: <ShoppingCar /> },
     { path: '/success', element: <Success /> },
+    { path: '/usuarios', element: <Usuarios /> },
     { path: '*', element: <NotFound /> },
   ])
   return routes;
